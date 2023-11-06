31e20e50cb32d063a765d8b556f90ba021668145
Yuri Duarte <gustavoyuripaulino@gmail.com>
2023-11-06 18:11:00 -0300

    usando massa de dados

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://lojaebac.ebaconline.art.br/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,39 +1,58 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
 
-context('funcionalidade login', () =>{
+context('funcionalidade login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
-   afterEach(() => {
+    afterEach(() => {
         cy.screenshot()
-   }); 
-   
-   
+    });
+
+
     it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.page-title').should('contain' , 'Minha conta')
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, aluno_ebac20')
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac20')
     })
 
-    it('Deve exibir uma menssagem de erro ao inserir usuário invalido', () =>{
+    it('Deve fazer login com sucesso - usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain', 'Minha conta')
+    });
+
+    it.only('Deve fazer login com sucesso - usando fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+
+            cy.get('.page-title').should('contain', 'Minha conta')
+        })
+    });
+
+    it('Deve exibir uma menssagem de erro ao inserir usuário invalido', () => {
         cy.get('#username').type('ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-error').should('contain' , 'Erro: a senha fornecida para o e-mail')
+        cy.get('.woocommerce-error').should('contain', 'Erro: a senha fornecida para o e-mail')
     })
 
-    it('Deve exibir uma menssagem de erro ao inserir senha invalida', () =>{
+    it('Deve exibir uma menssagem de erro ao inserir senha invalida', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-error').should('contain' , 'Erro: a senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error').should('contain', 'Erro: a senha fornecida para o e-mail aluno_ebac@teste.com está incorreta. Perdeu a senha?')
     })
 
 })
