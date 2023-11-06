451b298115c9b213a86d0d7ff888249584b4b7a7
Yuri Duarte <gustavoyuripaulino@gmail.com>
2023-11-06 17:07:56 -0300

    alteracoes no teste automatizado

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -2,8 +2,16 @@
 
 context('funcionalidade login', () =>{
 
-    it('Deve fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+   afterEach(() => {
+        cy.screenshot()
+   }); 
+   
+   
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -13,7 +21,6 @@ context('funcionalidade login', () =>{
     })
 
     it('Deve exibir uma menssagem de erro ao inserir usuário invalido', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
@@ -22,7 +29,6 @@ context('funcionalidade login', () =>{
     })
 
     it('Deve exibir uma menssagem de erro ao inserir senha invalida', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
---
+++ cypress/e2e/produtos.spec.cy.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade Página de Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            .first()
+            .click()
+    });
+
+    it('Deve adicionar um produto ao carrinho', () => {
+        var quantidade = 3
+        
+        cy.get('[class="product-block grid"]')
+            .first()
+            .click()
+        cy.get('.button-variable-item-L').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain' , quantidade)
+        cy.get('.woocommerce-message').should('contain' , quantidade + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+    });
+});
